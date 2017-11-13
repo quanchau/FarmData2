22237270df3919f2aaf15bda07af0f3563135c14
EagleShot <ashiraseesh@gmail.com>
2017-11-12 22:48:18 -0500

    Working on list view templates

--- farmdata2/settings.py
+++ farmdata2/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'farmdata2.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'farmdata2',
+        'USER': 'ashir',
+        'PASSWORD': 'mango2012',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
